20c6ad58bb3d54e88605dae19a7562a66c5702b3
SergeyBiryukov <SergeyBiryukov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-30 01:23:25 +0000

    git-svn-id: https://plugins.svn.wordpress.org/edit-category-slug/trunk@168590 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- edit-category-slug.php
+++ edit-category-slug.php
@@ -3,7 +3,7 @@
 Plugin Name: Edit Category Slug
 Version: 0.3
 Plugin URI: http://uplift.ru/projects/
-Description: Allows to edit a category slug in WordPress MU.
+Description: Allows to specify or edit a category slug in WordPress MU.
 Author: Sergey Biryukov
 Author URI: http://sergeybiryukov.ru/
 */
