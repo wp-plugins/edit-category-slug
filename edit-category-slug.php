d6cf0d793a3e6bec726c8d33d77551b8319b6053
SergeyBiryukov <SergeyBiryukov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-06 00:31:44 +0000

    added support for WordPress 3.0 Multisite
    git-svn-id: https://plugins.svn.wordpress.org/edit-category-slug/trunk@260615 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- edit-category-slug.php
+++ edit-category-slug.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Edit Category Slug
-Version: 0.4-trunk
+Version: 0.5-trunk
 Plugin URI: http://uplift.ru/projects/
 Description: Allows to specify or edit a category slug in WordPress MU.
 Author: Sergey Biryukov
@@ -9,19 +9,27 @@ Author URI: http://sergeybiryukov.ru/
 */
 
 function ecs_display_slug_row($category) {
+	$label = function_exists('_x') ? _x('Slug', 'Taxonomy Slug') : __('Category Slug');
+	$category_slug = apply_filters('editable_slug', $category->slug);
+	$slug = function_exists('esc_attr') ? esc_attr($category_slug) : attribute_escape($category_slug);
+
+	if ( version_compare( get_bloginfo('version'), '3.0', '>=' ) )
+		$form_id = array( 'add' => 'addtag', 'edit' => 'edittag' );
+	else
+		$form_id = array( 'add' => 'addcat', 'edit' => 'editcat' );
 ?>
 <script type="text/javascript">
 //<![CDATA[
-var form_addcat = document.getElementById('addcat');
-var form_editcat = document.getElementById('editcat');
+var form_addcat = document.getElementById('<?php echo $form_id['add']; ?>');
+var form_editcat = document.getElementById('<?php echo $form_id['edit']; ?>');
 var slugRow = '<tr class="form-field">' +
-	'<th scope="row" valign="top"><label for="category_nicename"><?php _e("Category Slug") ?></label></th>' +
-	'<td><input name="category_nicename" id="category_nicename" type="text" value="<?php echo attribute_escape(apply_filters("editable_slug", $category->slug)); ?>" size="40" /><br />' +
+	'<th scope="row" valign="top"><label for="category_nicename"><?php echo $label; ?></label></th>' +
+	'<td><input name="category_nicename" id="category_nicename" type="text" value="<?php echo $slug; ?>" size="40" /><br />' +
 	'<?php _e("The &#8220;slug&#8221; is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens."); ?></td>' +
 	'</tr>';
 var slugDiv = '<div class="form-field">' +
-	'<label for="category_nicename"><?php _e("Category Slug") ?></label>' +
-	'<input name="category_nicename" id="category_nicename" type="text" value="<?php echo attribute_escape(apply_filters("editable_slug", $category->slug)); ?>" size="40" />' +
+	'<label for="category_nicename"><?php echo $label; ?></label>' +
+	'<input name="category_nicename" id="category_nicename" type="text" value="" size="40" />' +
 	'<p><?php _e("The &#8220;slug&#8221; is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens."); ?></p>' +
 	'</div>';
 if ( form_addcat != null )
@@ -34,10 +42,16 @@ if ( form_editcat != null )
 }
 add_action('edit_category_form', 'ecs_display_slug_row');
 
-function ecs_edit_slug($cat_ID) {
+function ecs_edit_slug($cat_id) {
 	global $wpdb;
-	if ( !empty($_POST['category_nicename']) )
-		$wpdb->update( $wpdb->terms, array( 'slug' => $_POST['category_nicename'] ), array( 'term_id' => $cat_ID ) );
+	if ( isset($_POST['category_nicename']) ) {
+		if ( !empty($_POST['category_nicename']) ) 
+			$category_nicename = function_exists('esc_html') ? esc_html($_POST['category_nicename']) : wp_specialchars($_POST['category_nicename']);
+		else
+			$category_nicename = sanitize_title($_POST['name']);
+
+		$wpdb->update( $wpdb->terms, array( 'slug' => $_POST['category_nicename'] ), array( 'term_id' => $cat_id ) );
+	}
 }
 add_action('edit_category', 'ecs_edit_slug');
 
